0ae4abcf956aae79fdb6aeafc04736d5dc0a1590
Roberto Mario Buelvas <robertobuelvas94@gmail.com>
2019-07-04 10:47:37 -0400

    Added test of GPS

---
+++ GPSTest.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 24 12:48:43 2019
+
+@author: ROBERTO MARIO
+"""
+
+import serial
+import pynmea2
+
+serialCropCircle=serial.Serial('COM17',9600)
+for i in range(10):
+    message=serialCropCircle.readline().strip()
+    parsedMessage=pynmea2.parse(message)
+    print(parsedMessage.longitude)
+    print(parsedMessage.latitude)
+serialCropCircle.close()
